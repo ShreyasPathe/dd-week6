cbf4d18788df450872f08f2a7a41bf082bd6677f
ShreyasPathe <patheshreyas7@gmail.com>
2026-01-04 14:54:39 +0530

    Fix doLogout export error in logout route

--- app/routes/($locale).account_.logout.ts
+++ app/routes/($locale).account_.logout.ts
@@ -1,13 +1,16 @@
-import { redirect, type LoaderFunctionArgs } from '@shopify/remix-oxygen';
+import { redirect, type LoaderFunctionArgs, type AppLoadContext } from '@shopify/remix-oxygen';
 
-export async function action({ context }: LoaderFunctionArgs) {
+export async function doLogout(context: AppLoadContext) {
   const shopDomain = context.env.PUBLIC_STORE_DOMAIN;
   return redirect(`https://${shopDomain}/account/logout`);
 }
 
+export async function action({ context }: LoaderFunctionArgs) {
+  return doLogout(context);
+}
+
 export async function loader({ context }: LoaderFunctionArgs) {
-  const shopDomain = context.env.PUBLIC_STORE_DOMAIN;
-  return redirect(`https://${shopDomain}/account/logout`);
+  return doLogout(context);
 }
 
 export default function Logout() {
